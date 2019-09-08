37cbc4173cfc60a63ea24ddc628627f8cb685b85
Mark Johnson <markjohnson303@gmail.com>
2019-09-08 16:31:00 -0600

    alternate turns

--- src/index.js
+++ src/index.js
@@ -3,22 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { throwStatement, arrayExpression } from "@babel/types";
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        })
   }
 
   renderSquare(i) {
@@ -31,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
