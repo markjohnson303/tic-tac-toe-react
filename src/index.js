5978cda4a2fb6d8cb8c7da2ad8f10cab2de96a5f
Mark Johnson <markjohnson303@gmail.com>
2019-09-08 16:04:19 -0600

    board handles state

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { throwStatement, arrayExpression } from "@babel/types";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button 
-      className="square" 
-      onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
